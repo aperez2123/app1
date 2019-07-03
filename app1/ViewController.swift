e7e6d75f298aaadc578522f018cd10cee4f9a5cc
Alexis <aperez50759@gamil.com>
2019-07-03 13:05:16 -0500

    Initial Commit

---
+++ app1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  app1
+//
+//  Created by SER-YCL8 on 7/3/19.
+//  Copyright © 2019 SER-YCL8. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
